de28969cc47bfff90f2f69dcef516e28838b6d18
root <root@kali.yamato>
2023-04-07 16:21:00 +0200

    title has been modified

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my first React app on Netlify
         </p>
+        <p>{props.name}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React
         </a>
       </header>
     </div>
